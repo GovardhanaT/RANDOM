744d85d9502191157ecfc149ad516334de7d2e7c
Govardhana <113413304+GovardhanaT@users.noreply.github.com>
2023-05-26 09:32:35 +0530

    fin

--- EducationalLoanPortal/src/main/java/com/EducationalLoanPortal/app/Repositories/UserModelRepo.java
+++ EducationalLoanPortal/src/main/java/com/EducationalLoanPortal/app/Repositories/UserModelRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.EducationalLoanPortal.app.Models.UserModel;
 
-public interface UserModelRepo extends JpaRepository<UserModel, Integer> {
+public interface UserMod elRepo extends JpaRepository<UserModel, Integer> {
 UserModel findByEmail(String email);
 }
